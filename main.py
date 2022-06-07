8c97d05ae46cdade37cf5582dbb82b92e8a00b03
Jefferson <jeffersonmontilla.98@gmail.com>
2022-06-06 21:32:46 -0400

    Implement the CLI

--- main.py
+++ main.py
@@ -1,9 +1,24 @@
+import argparse
 from PIL import Image
 
 
+def cli():
+    parser = argparse.ArgumentParser(description="Convert image to jpg")
+    parser.add_argument("-i", "--input", help="Input image", required=True)
+    parser.add_argument("-o", "--output", help="Output image", required=True)
+    parser.add_argument("-q", "--quality", help="Quality of output image", required=True)
+    
+    return parser.parse_args()
+
+
 def run():
-    with Image.open("test.jpg") as img:
-        img.convert("RGB").save("test_picture.jpg", quality=30)
+    args = cli()
+
+    img = Image.open(args.input)
+    img.convert("RGB").save(args.output, quality=int(args.quality))
+
+    #with Image.open("test.jpg") as img:
+    #    img.convert("RGB").save("test_picture.jpg", quality=30)
 
 
 if __name__ == "__main__":
