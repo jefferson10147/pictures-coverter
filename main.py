f2efe558fa0ebff8449428e809e0709785553665
Jefferson <jeffersonmontilla.98@gmail.com>
2022-06-13 00:17:55 -0400

    Define one funtion to convert all files from a folder

--- main.py
+++ main.py
@@ -15,22 +15,23 @@ def compress_file(input_file, output_file, input_folder, output_folder, quality)
         output_folder + output_file, "JPEG", quality=int(quality))
 
 
-def compress_all_files(path, extension=".jpg", save_path="./", quality=30):
-    for file in os.listdir(path):
+def convert_all_files(input_folder, output_folder, extension, final_extension, quality):
+    for file in os.listdir(input_folder):
         if file.endswith(extension):
-            compress_file(file, save_path, quality)
+            print("Compressing file: " + file)
+            compress_file(file, file.replace(extension, final_extension), input_folder, output_folder, quality)
 
 
 def cli():
     parser = argparse.ArgumentParser(description="Convert image to jpg")
-    parser.add_argument("-i", "--input", help="Input image", required=True)
-    parser.add_argument("-o", "--output", help="Output image", required=True)
+    parser.add_argument("-i", "--input", help="Input image", required=False)
+    parser.add_argument("-o", "--output", help="Output image", required=False)
     parser.add_argument("--input_folder", help="Input folder", required=False, default="./")
     parser.add_argument("--output_folder", help="Output folder", required=False, default="./")
     parser.add_argument("-q", "--quality", help="Quality of output image", required=False, default=30)
-    
-    # parser.add_argument("-a", "--all", help="Compress all files in directory", action="store_true")
-    # parser.add_argument("-e", "--extension", help="Extension of files to compress", default=".jpg")
+    parser.add_argument("-a", "--all", help="Compress all files in directory", action="store_true")
+    parser.add_argument("-e", "--extension", help="Extension of files to compress", default=".jpg")
+    parser.add_argument("--final_extension", help="Extension of files to compress", default=".png")
 
     return parser.parse_args()
 
@@ -38,13 +39,11 @@ def cli():
 def main():
     args = cli()
 
-    # if args.all:
-    #    compress_all_files(args.input, args.extension, args.output, args.quality)
-    # elif args.cjpg:
-    #    convert_from_jpg_to_png(args.input, args.output)
-    # else:
-    
-    compress_file(args.input, args.output, args.input_folder, args.output_folder, args.quality)
+    if args.all:
+        convert_all_files(
+            args.input_folder, args.output_folder, args.extension, args.final_extension, args.quality)
+    else:
+        compress_file(args.input, args.output, args.input_folder, args.output_folder, args.quality)
     
     
 if __name__ == "__main__":
